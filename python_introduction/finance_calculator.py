99d1b38dd2ce40b14a2bd9175e1607cc1f32d84d
Moses Mugaya Mashengu <mugayastudies@gmail.com>
2025-05-21 17:36:27 +0300

    Fix: Total_monthly_expenses to monthly_expenses in finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,12 +2,10 @@
 # and potential future savings without applying conditional statements.
 
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
-print (f"Your monthly savings are {monthly_savings}. ")
-
+print(f"Your monthly savings are ${monthly_savings}.")
 projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-
-print (f"Your projected savings after one year with interest, is: {projected_annual_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings}.")
