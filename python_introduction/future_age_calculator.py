b5f61dbbb22655534dfd5f7fa02aeda0f59003e2
Moses Mugaya Mashengu <mugayastudies@gmail.com>
2025-05-21 16:08:19 +0300

    Fix: what is your current age? to How old are you? in future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
 # Objective: Practice receiving user input in Python and perform a simple arithmetic operation to calculate the user’s age in a future year.
 
-Current_age = int(input("What is your current age? "))
+Current_age = int(input("How old are you? "))
 
 Future_age = Current_age + 27
 
