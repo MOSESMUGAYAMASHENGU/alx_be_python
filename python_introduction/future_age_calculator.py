67b1835739935e756fae0ecb325694b53867ed7d
Moses Mugaya Mashengu <mugayastudies@gmail.com>
2025-05-21 15:59:02 +0300

    Add Task 4: future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+# Objective: Practice receiving user input in Python and perform a simple arithmetic operation to calculate the user’s age in a future year.
+
+Current_age = int(input("What is your current age? "))
+
+Future_age = Current_age + 27
+
+print (f"In 2050, you will be {Future_age} years old.")
+
+
